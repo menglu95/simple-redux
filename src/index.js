6e7851a5816c59110b83adde27118305f39aa4c0
LuMeng9 <LuMeng9@outlook.com>
2020-06-16 20:12:41 +0800

    simple redux example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore } from "redux";
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCHER
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
