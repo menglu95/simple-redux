92bb71811ad4768879433352eaf4929305e6bad0
LuMeng9 <LuMeng9@outlook.com>
2020-06-16 22:38:32 +0800

    learning react-redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const logged = useSelector(state => state.logged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>PLUS</button>
+      <button onClick={() => dispatch(decrement())}>MINUS</button>
+      {logged ? <h2>Hello!</h2> : <h2>Login</h2>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,49 +5,18 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
-// STORE -> GLOBALIZED STATE
-
-// ACTION INCREMENT
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  };
-};
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCHER
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
+const myStore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // reference: https://github.com/zalmoxisus/redux-devtools-extension#usage
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
